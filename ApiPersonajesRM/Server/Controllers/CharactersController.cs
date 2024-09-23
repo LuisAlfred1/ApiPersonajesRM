79a83aa675504528c4330d04c8808087567455d2
asus <asus@DESKTOP-EOF8MPJ>
2024-09-22 18:27:26 -0600

    Agregar archivos de proyecto.

---
+++ ApiPersonajesRM/Server/Controllers/CharactersController.cs
@@ -0,0 +1,51 @@
+﻿using ApiPersonajesRM.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiPersonajesRM.Server.Controllers
+{
+    [Route("api/characters")]
+    public class CharactersController : Controller
+    {
+        private HttpClient _httpClient;
+        public CharactersController()
+        {
+            _httpClient = new HttpClient();
+            
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<Characters> GetAllCharacters(int page, string name = "")
+        {
+            try
+            {
+                Characters characters = null;
+
+                //Construye la url de la Api con los parametros de la pagina y nombre
+                string url = $"https://rickandmortyapi.com/api/character?page={page}";
+
+                if (!string.IsNullOrEmpty(name))
+                {
+                    url += $"&name={name}";
+                }
+
+                HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync(url);
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await
+                    httpResponseMessage.Content.ReadAsStringAsync();
+
+
+                characters = JsonConvert.DeserializeObject<Characters>(responseBody);
+
+                return characters;
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
